ee40889efa486f0b15ef9e21fc5076cea146ad5e
Reo Hakuta <reonaldhkt+webs@gmail.com>
2022-12-07 15:17:36 +0900

    feat: ✨ (提出課題17-5) 7セグメント出力付きの4bitカウンタを作成した。

---
+++ 17-5_counter-with-seven-segment/TopModule.v
@@ -0,0 +1,37 @@
+module TopModule(
+	//////////// CLOCK //////////
+	input 		          		CLK1,
+	input 		          		CLK2,
+	//////////// SEG7 //////////
+	output		     [7:0]		HEX0,
+	output		     [7:0]		HEX1,
+	output		     [7:0]		HEX2,
+	output		     [7:0]		HEX3,
+	output		     [7:0]		HEX4,
+	output		     [7:0]		HEX5,
+	//////////// Push Button //////////
+	input 		     [1:0]		BTN,
+	//////////// LED //////////
+	output		     [9:0]		LED,
+	//////////// SW //////////
+	input 		     [9:0]		SW
+
+	);
+	wire clk, res, wnq;
+	wire [3:0] wq;
+	
+	m_rs_flipflop u1(BTN[0], BTN[1], clk, wnq);	// ボタン信号からクロック信号を生成
+	
+	assign res = SW[0];			// リセット用のボタン信号
+	
+	m_counter(clk, res, wq);	// 4bitカウンタ
+	
+	assign LED={6'h0,wq};
+	m_seven_segment(wq, HEX0);
+	assign HEX1=8'hff;
+	assign HEX2=8'hff;
+	assign HEX3=8'hff;
+	assign HEX4=8'hff;
+	assign HEX5=8'hff;
+	
+endmodule
